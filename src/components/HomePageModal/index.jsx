be3f4cded4d964a4ccdbdf794dc14ddf3c00c63e
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-12-01 11:45:50 -0800

    updated homepage layout

--- src/components/HomePageModal/index.jsx
+++ src/components/HomePageModal/index.jsx
@@ -1,15 +1,43 @@
 import './index.css';
+import LinkedInIcon from '../../assets/LinkedIn-icon.jpg';
+import GithubIcon from '../../assets/Github-Icon.png';
+import GmailIcon from '../../assets/gmail-icon.png';
 
 function HomePageModal() {
     return (
-        <div className='HomePageModal'>
-            <div className="h1Container">
-                <h1>Justin Feagin</h1>
+        <div>
+            <div className='HomePageModal'>
+                <div className="ContentContainer">
+                    <div className='TitleContainer'>
+                        <h1>
+                            Justin
+                            <br />
+                            Feagin
+                        </h1>
+                    </div>
+                    <div className='ParagraphContainer'>
+                        <p>Web Developer</p>
+                        <p>HTML, CSS, JavaScript and React</p>
+                        <p>Bachelors of Science in Business Administration</p>
+                    </div>
+                </div>
             </div>
-            <div className="pContainer">
-                <p>Web Developer</p>
+            <div className='ImageLinksContainer'>
+                <div className='icon'>
+                    <img src={LinkedInIcon} alt="LinkedIn" />
+                    <p>LinkedIn</p>
+                </div>
+                <div className='icon'>
+                    <img src={GithubIcon} alt="github" />
+                    <p>GitHub</p>
+                </div>
+                <div className='icon'>
+                    <img src={GmailIcon} alt="gmail" />
+                    <p>Gmail</p>
+                </div>
             </div>
         </div>
+
     )
 }
 
