2b970b4920fbc4f8802488c31c9555218e6a8bde
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-12-04 13:48:56 -0800

    added my projects to the projects page, bug fixes

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -8,10 +8,10 @@ function AboutMe() {
                 <h1>About Me:</h1>
                 <img src={ProfilePicture} alt="PFP" />
                 <p>
-                    I am a budding Web Developer with a Bachelors of Science Degree in Management. 
-                    <br />
-                    I have recently earned a certificate in full stack development from University of California San Diego. In this program I developed skills in JavaScript, CSS, HTML, React.js, and more.
+                    I am a budding Web Developer with a Bachelors of Science Degree in Management from Clemson University, South Carolina. 
                 </p>
+                    <br />
+                <p>I have recently earned a certificate in full stack development from University of California San Diego. In this program I developed skills in JavaScript, CSS, HTML, React.js, and more.</p>
                     <br />
                 <p>
                     I used to be in the sales industry but ended up moving to California to try and break into a new industry. I am very passionate about computers, programming, and web development. I am also passionate about gaming, and it is my hope to one day break into the gaming industry as a developer of some sort.
--- src/components/ProjectsModal/index.jsx
+++ src/components/ProjectsModal/index.jsx
@@ -1,8 +1,48 @@
+import MovieFinder from '../../assets/MovieFinderScreenshot.png'
+import CalendarPlanner from '../../assets/Calendar-Planner.png'
+import PasswordGenerator from '../../assets/PasswordGenerator.png'
+import WorkDayScheduler from '../../assets/WorkDayScheduler.png'
+import TournamentMaker from '../../assets/TournamentMaker.png'
 import './index.css'
 
 function ProjectsModal() {
     return (
-       <p>hello world!</p>
+       <div className='EntireProjectsPage'>
+            <div className='Projects'>
+                <h1>Projects</h1>
+                <p>
+                    Here are some Projects I have worked on over the course of the past few months.
+                    <br />
+                    I will be updating this section of my Portfolio regularly as I complete new projects over time!
+                </p>
+                <div className='completedProjects'>
+                    <h2>Movie Finder: Find Movies on any Streaming Platform!</h2>
+                    <a href="https://matthkang.github.io/Movie-Finder/">
+                        <img src={MovieFinder} alt="MovieFinder" />
+                    </a>
+                    <h2>Calendar Planner: Plan well into the future!</h2>
+                    <a href="https://protected-taiga-58244-3710fb729053.herokuapp.com/">
+                        <img src={CalendarPlanner} alt="CalendarPlanner" />
+                    </a>
+                    <h2>Password Generator: Creating Random Passwords is Easy!</h2>
+                    <a href="https://cjfeagin33.github.io/Random-Password-Generator/">
+                        <img src={PasswordGenerator} alt="Password Generator" />
+                    </a>
+                    <h2>Work Day Scheduler: Plan Each Hour With Ease!</h2>
+                    <a href="https://cjfeagin33.github.io/Calendar-Application/">
+                        <img src={WorkDayScheduler} alt="WorkDayScheduler" />
+                    </a>
+                    <h2>Tournament Maker (Work In Progress):
+                        <br />
+                        Create Tournaments for your favorite video games!
+                    </h2>
+                    <a href="https://game-tourney-hub-8015c3047dd6.herokuapp.com/">
+                        <img src={TournamentMaker} alt="TournamentMaker" />
+                    </a>
+                </div>
+                <p>More projects coming soon!</p>
+            </div>
+       </div>
     );
 }
 
