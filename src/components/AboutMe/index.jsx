7efa31e601cb391659fe242f37cec8c529933002
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-12-07 20:06:07 -0800

    updated about me section

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -14,7 +14,7 @@ function AboutMe() {
                 <p>I have recently earned a certificate in full stack development from University of California San Diego. In this program I developed skills in JavaScript, CSS, HTML, React.js, and more.</p>
                     <br />
                 <p>
-                    I used to be in the sales industry but ended up moving to California to try and break into a new industry. I am very passionate about computers, programming, and web development. I am also passionate about gaming, and it is my hope to one day break into the gaming industry as a developer of some sort.
+                    I used to be in the sales industry but ended up moving to California to try and break into a new industry. I am very passionate about computers, programming, and web development. I am also passionate about gaming, and it is my hope to one day break into the gaming industry as a developer.
                 </p>
                 <br />
                 <p>If you would like to reach out to me for employment or collaborative purposes, please visit my Contact Me section on this Portfolio!</p>
